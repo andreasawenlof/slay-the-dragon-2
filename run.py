f0e2540c2f1b45e3b45565401d2cdf7ea11f3763
mezter1337 <andreas.javert@gmail.com>
2024-04-12 14:59:31 +0200

    Import JSON-file into run.py

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+1. Import game.json
+2. Select the starting room
+3. Print the rooms description
+4. Print the rooms exit-names and descriptions
+"""
+
+import json

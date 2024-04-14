95dff9cb9e9407f38de454334f6503073a9123ba
mezter1337 <andreas.javert@gmail.com>
2024-04-15 00:13:02 +0200

    refactor hard coding to dynamic coding with variables

--- run.py
+++ run.py
@@ -2,10 +2,7 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 """
-1. Import game.json
-2. Select the starting room
-3. Print the rooms description
-4. Print the rooms exit-names and descriptions
+
 """
 
 import json
@@ -17,7 +14,8 @@ with open('./assets/game.json') as f:
 print('Starting Room', game_data['starting_room'])
 
 rooms_data = game_data['rooms']
-room1 = rooms_data['room1']
+room_key = game_data['starting_room']
+current_room = rooms_data[room_key]
 
 # Function for printing out a selected room
 def print_room(room):
@@ -27,5 +25,5 @@ def print_room(room):
         print(exit['name'])
         print(exit['description'])
 
-print_room(room1)
+print_room(current_room)
 

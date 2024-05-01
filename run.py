cb333ed44189a09687e5d41b5c7513e34dae4dee
mezter1337 <andreas.javert@gmail.com>
2024-05-01 19:30:49 +0200

    Add take item feature and split it up in functions

--- run.py
+++ run.py
@@ -3,10 +3,8 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 """
 1. Show items in the current room (check)
-2. Pickup/Take the items in the current room
+2. Pickup/Take the item in the current room
 3. Action to print inventory
-<<<<<<< HEAD
-=======
 
 2. Action to print inventory
 
@@ -19,7 +17,6 @@ Match item object with found item key
 Remove item_key from room item array
 Add item_key to Player Inventory
 
->>>>>>> 6b16273 (Add inventory feature to check invetory)
 """
 import json
 from prompt_toolkit import prompt
@@ -34,11 +31,12 @@ def run_game():
     rooms_data = game_data['rooms']
     room_key = game_data['starting_room']
     current_room = rooms_data[room_key]
+    current_room_items = rooms_data[room_key]['items']
 
     while True:
+      answer = prompt('What to do?: ')
       print_room(current_room)
       print_item(current_room)
-      answer = prompt('What to do?: ')
       
       #Got this solution by stackflow, see readme
       action = answer.split(maxsplit=1)
@@ -48,38 +46,26 @@ def run_game():
           print('Quitting the game..... byeeeeee!')
           break
       
-      if action[0] == 'inventory' or action[0] == 'inv':
-          print('checking the inventory...')
+      elif action[0] == 'inventory' or action[0] == 'inv':
+          print('Checking the inventory...')
           print(game_data['player']['inventory'])
       
-      if(action[0] == 'walk'):
+      elif(action[0] == 'walk'):
         exit = select_exit(action[1], current_room)
         current_room = room_from_exit(exit, rooms_data)
       
-      if(action[0] == 'take'):
-          take_item = take_item()
-          
+      elif(action[0] == 'take'):
+          item = take_item(action[1], current_room)
 
-      elif (action[0] == 'take'):
           
 
 # Function for printing out a selected room
-def print_room(room):sfaf
+def print_room(room):
     print(room['description'])
-    room_items = room['items']
     room_exits = room['exits']
     for exit in room_exits:
         print(exit['name'])
         print(exit['description'])
-    
-    items = game_data['items'] 
-    for item_key in room_items:
-        print(f'Item: {items[item_key]['name']}')
-        print(f'Usage: {items[item_key]['description']}')
-    
-"""
-
-"""
 
 def print_item(room):
     room_items = room['items']
@@ -90,18 +76,39 @@ def print_item(room):
         print(item_name)
         print(item_desc)
 
-def take_item(item_name, inventory, room):
+def take_item(item_name, room, player):
+    item_key = get_item_key_from_item_name(item_name)
+    remove_item_key_from_room_items(item_key, room)
+    add_item_key_to_player_inventory(item_key, player)
+
+    
+
+
+    """
     room_item_list = game_data['rooms'][room]['items']
     room_items = game_data['items']
     player_inventory = game_data['player']['inventory']
     for key in game_data['items']:
         room_item_name = room_items[key]['name']
-        if room_item_name == 'item_name':
+        if room_item_name == item_name:
             room_item_list.remove(key)
             player_inventory.append(key)
+    return item_name;
+    """
 
+def get_item_key_from_item_name(item_name):
+    return 'sword' #key
 
+def remove_item_key_from_room_items(item_key, room):
     
+    return
+
+def add_item_key_to_player_inventory(item_key, player):
+    return
+
+
+
+
 
 
 
@@ -127,8 +134,6 @@ def room_from_exit(exit, rooms_data):
     exit_destination = exit['destination']
     room = rooms_data[exit_destination]
     return room
-
-
 
 run_game()
 
